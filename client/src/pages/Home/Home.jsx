da190f033594d28dcc46b6e5975f564bb4cc9928
yash suthar <ysuthar8055@gmail.com>
2023-11-01 17:26:44 +0530

    home page completed

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,24 +1,32 @@
-// import React from "react";
-// import QrReader from 'react-qr-reader'
+import React from "react";
 
 const Home = () => {
   return (
-    <div className="bg-blue-100">
-      <div className=" h-screen flex justify-around items-center">
-        <div className="p-5 flex-col justify-center items-center text-center font-sans">
-          <p>Welcome to Anubhuti !</p>
-          <p className="text-3xl font-bold">Gujrat Police feedback system</p>
-
-          <div className="bg-white p-10 border-0 rounded-xl shadow-xl block-none">
-            <p className=" font-medium text-xl">Scan QR-code</p>
-            <img className="w-36 " src="https://www.askdavetaylor.com/wp-content/uploads/2021/02/chrome-webpage-qr-code-3-291x375.jpg" alt="" />
-          </div>
+    <div className="bg-blue-100 h-screen flex justify-evenly items-center max-sm:flex-col-reverse pt-20">
+      <div className="p-5 flex-col justify-center items-center text-center font-sans">
+        <div className="my-8">
+          <p className="my-1">Welcome to Anubhuti!</p>
+          <p className="text-3xl max-sm:text-2xl font-bold">Gujarat Police feedback system</p>
         </div>
-        <div className="text-center font-semibold text-3xl">
 
-          <img src="https://gujhome.gujarat.gov.in/portal/images/Home/gujaratpolice.png" className="w-48 h-48" alt="" />
-          <p>Gujrat Police</p>
+        <div className="bg-white mx-auto w-64 p-10 border-0 rounded-xl shadow-xl">
+          <p className="font-medium text-xl">Scan QR-code</p>
+          <img
+            className="w-36 mx-auto"
+            src="https://www.askdavetaylor.com/wp-content/uploads/2021/02/chrome-webpage-qr-code-3-291x375.jpg"
+            alt=""
+          />
         </div>
+
+        <button className="w-64 px-5 py-2 my-5 border-0 rounded-xl transition duration-200 shadow-lg text-white font-semibold hover:bg-blue-900 bg-blue-600">Give Your Feedback</button>
+      </div>
+      <div className="text-center font-semibold text-3xl flex-col justify-center items-center">
+        <img
+          src="https://gujhome.gujarat.gov.in/portal/images/Home/gujaratpolice.png"
+          className="mx-auto w-48 h-48 max-sm:w-20 max-sm:h-20 "
+          alt=""
+        />
+        <p className="max-sm:text-xl">Gujarat Police</p>
       </div>
     </div>
   );
