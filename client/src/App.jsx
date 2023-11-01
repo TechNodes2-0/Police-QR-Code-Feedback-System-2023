29284f44c7d024ec6da9165857235d89b3c0bc05
NishitBaria <vinayakvispute262003@gmail.com>
2023-11-01 20:38:45 +0530

    Fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,9 @@ import "./App.css";
 import NotFound from "./pages/Error/NotFound";
 import Home from "./pages/Home/Home";
 import Navbar from "./components/Navbar";
-<<<<<<< HEAD
 import Footer from "./components/Footer";
-
-=======
 import Form from "./pages/Forms/Form";
 import QRCodeGenerator from "./test/QRCodeGenerator";
->>>>>>> b4b3491a4b3ce646226622a023e5180ddfe3a32c
 function App() {
   return (
     <div className="flex min-h-screen w-screen flex-col bg-richblack-900 font-inter">
@@ -22,17 +18,11 @@ function App() {
         <Route path="/test">
           <Route index element={<QRCodeGenerator />} />
         </Route>
-
         {/* ------------Test End--------- */}
-
         <Route path="*" element={<NotFound />} />
       </Routes>
-<<<<<<< HEAD
-      <Footer/>
-    </>
-=======
+      <Footer />
     </div>
->>>>>>> b4b3491a4b3ce646226622a023e5180ddfe3a32c
   );
 }
 
