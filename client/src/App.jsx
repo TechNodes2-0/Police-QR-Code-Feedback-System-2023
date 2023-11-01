61db9cc8997558f3a8db3dc7259ae664d887b3dc
NishitBaria <vinayakvispute262003@gmail.com>
2023-11-01 11:46:29 +0530

    Home Routes Updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes as Router, Route } from "react-router-dom";
 import "./App.css";
 import NotFound from "./pages/Error/NotFound";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <>
       <Router>
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Router>
     </>
