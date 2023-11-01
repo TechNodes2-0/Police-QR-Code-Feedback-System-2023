efdb29958cce579ea674f1247fe59129fcf0fc5c
NishitBaria <vinayakvispute262003@gmail.com>
2023-11-01 21:07:47 +0530

    Server File Structure Updated

---
+++ server/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connectDB = require("./db/connect");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const fileUpload = require("express-fileupload");
+const cloudinary = require("./db/cloudinary");
+require("dotenv").config();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+// Middleware
+app.use(express.json());
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+// Error handler middleware
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+cloudinary.cloudinaryConnect();
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
