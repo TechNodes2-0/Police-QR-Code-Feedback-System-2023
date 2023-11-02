35979e3eafaeb40a31a912d7305014007800a8a2
JayeshYadav99 <jayeshyadav528@gmail.com>
2023-11-02 10:08:18 +0530

    Fixing Firebase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,8 @@ import Home from "./pages/Home/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import Form from "./pages/Forms/Form";
 import QRCodeGenerator from "./test/QRCodeGenerator";
 import PhoneAuth from "./PhoneAuth";
--- client/src/PhoneAuth.jsx
+++ client/src/PhoneAuth.jsx
@@ -23,7 +23,7 @@ const uiConfig = {
   // Popup signin flow rather than redirect flow.
   signInFlow: 'popup',
   // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: '/signedIn',
+  signInSuccessUrl: '/form',
   // We will display Google and Facebook as auth providers.
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
