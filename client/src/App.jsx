8d62af7f27833c8bb9479c4720ccc012ba13a400
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-11-03 01:12:20 +0530

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React ,{useState,useEffect} from "react";
+import { Suspense, lazy, useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
@@ -6,34 +6,46 @@ import NotFound from "./pages/Error/NotFound";
 
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import firebase from 'firebase/compat/app';
+import firebase from "firebase/compat/app";
+import Loader from "../src/components/common/index";
+import { Toaster } from "react-hot-toast";
+import ECommerce from "./pages/Dashboard/ECommerce";
+import DefaultLayout from "./layout/DefaultLayout";
 
 import Form from "./pages/Forms/Form";
 import QRCodeGenerator from "./test/QRCodeGenerator";
-import { onAuthStateChanged, signOut,getAuth } from "firebase/auth";
+import { onAuthStateChanged, signOut, getAuth } from "firebase/auth";
 import FeedbackForm from "./components/core/Form/FeedbackForm";
 import PhoneAuth from "./components/core/Form/PhoneAuth";
 import Login from "./pages/Login/login";
 import Admin from "./pages/Admin/Admin";
-import DefaultLayout from "./layout/DefaultLayout";
-import ECommerce from "./pages/Dashboard/ECommerce";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    // Simulate loading
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
 
- 
-  
-
-  return (
+  return loading ? (
+    <Loader />
+  ) : (
     <div className="flex min-h-screen w-screen flex-col bg-blue-100 font-inter">
       {/* <PhoneAuth/> */}
-     
-     
+
       <Routes>
         <Route path="/form" element={<Form />} />
-        <Route path="/feedback" element={<FeedbackForm/>} ></Route>
+        <Route path="/feedback" element={<FeedbackForm />}></Route>
         <Route path="/" element={<PhoneAuth />} />
-        <Route path="/login" element={<Login/>} />
+        <Route path="/login" element={<Login />} />
         {/*Don't Touch this Route*/}
-        <Route path="/dashboard" element={<DefaultLayout />} />
+        
+        <Route path="/dashboard" element={<DefaultLayout />}>
+          <Route index element={<ECommerce />} />
+        </Route>
+
         {/* ------------Test--------- */}
         <Route path="/test">
           <Route index element={<QRCodeGenerator />} />
