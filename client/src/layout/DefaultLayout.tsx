0032eb2bd264edffd533de3900a0283f9a6a4492
yash suthar <ysuthar8055@gmail.com>
2023-11-03 18:31:38 +0530

    logout button

--- client/src/layout/DefaultLayout.tsx
+++ client/src/layout/DefaultLayout.tsx
@@ -1,22 +1,29 @@
-import React, { useState } from 'react';
-import Header from '../components/Header';
-import Sidebar from '../components/core/Admin/Sidebar';
-import { Outlet } from 'react-router-dom';
+import React, { useState } from "react";
+import Header from "../components/Header";
+import Sidebar from "../components/core/Admin/Sidebar";
+import { Outlet } from "react-router-dom";
 
 function DefaultLayout() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="dark:bg-boxdark-2 dark:text-bodydark">
-      <div className="flex h-screen overflow-hidden">
-        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-          <main>
-            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-              <Outlet />
-            </div>
-          </main>
+    <div className="">
+      <div className="py-2 relative right-0 w-full flex justify-end px-5">
+        <button className="border-0 rounded-lg py-1 px-3 translate transition duration-200 bg-blue-2 hover:bg-blue-3 hover:shadow-xl text-white font-bold">
+          logout
+        </button>
+      </div>
+      <div className="dark:bg-boxdark-2 dark:text-bodydark">
+        <div className="flex h-screen overflow-hidden">
+          <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+          <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+            <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+            <main>
+              <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                <Outlet />
+              </div>
+            </main>
+          </div>
         </div>
       </div>
     </div>
