71bf24c91231fe290eac27268fd0c753e7c8e9b5
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-11-04 15:13:02 +0530

    Change Route

--- client/src/components/ChartThree.jsx
+++ client/src/components/ChartThree.jsx
@@ -113,10 +113,10 @@ function ChartThree() {
   };
 
   return (
-    <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pt-7.5 pb-5 shadow-default dark:border-strokedark dark-bg-boxdark sm-px-7.5 xl-col-span-5">
+    <div className="col-span-12 rounded-sm text-white border border-stroke bg-darkgray px-5 pt-7.5 pb-5 shadow-default dark:border-strokedark dark-bg-boxdark sm-px-7.5 xl-col-span-5">
       <div className="mb-3 justify-between gap-4 sm-flex">
         <div>
-          <h5 className="text-xl font-semibold text-black dark-text-white">
+          <h5 className="text-xl font-semibold text-white dark:text-white">
             Response Analytics
           </h5>
         </div>
@@ -125,7 +125,7 @@ function ChartThree() {
             <select
               name=""
               id=""
-              className="relative z-20 inline-flex appearance-none bg-transparent py-1 pl-3 pr-8 text-sm font-medium outline-none"
+              className="relative z-20 inline-flex appearance-none bg-transparent py-1 pl-3 pr-8 text-sm font-medium outline-none border border-stroke text-white dark:text-white dark:border-strokedark rounded-md"
               onChange={(e) => setSelectedQuestionIndex(e.target.value)}
               value={selectedQuestionIndex}
             >
@@ -135,7 +135,7 @@ function ChartThree() {
                 </option>
               ))}
             </select>
-            <span className="absolute top-1/2 right-3 z-10 -translate-y-1/2">
+            <span className="absolute top-1/2 right-3 z-10 pointer-events-none">
               <svg
                 width="10"
                 height="6"
@@ -167,11 +167,10 @@ function ChartThree() {
               <span
                 className={`mr-2 block h-3 w-full max-w-3 rounded-full bg-primary`}
               ></span>
-              <p className="flex w-full justify-between text-sm font-medium text-black dark-text-white">
-                <span> {option.label} </span>
+              <p className="flex w-full justify-between text-sm font-medium text-white dark:text-white">
+                <span>{option.label}</span>
                 <span>
-                  {" "}
-                  {option.label} received {selectedResponses[index]} responses.{" "}
+                  {option.label} received {selectedResponses[index]} responses.
                 </span>
               </p>
             </div>
