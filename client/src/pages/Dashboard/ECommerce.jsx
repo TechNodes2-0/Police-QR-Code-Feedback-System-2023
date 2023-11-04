a8e59fbb16176116ec24641114d332e84d0629e9
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-11-04 16:38:51 +0530

    fix:Name

--- client/src/pages/Dashboard/ECommerce.jsx
+++ client/src/pages/Dashboard/ECommerce.jsx
@@ -17,9 +17,10 @@ const ECommerce = () => {
       </div>
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-        <ChartOne />
-        <ChartTwo />
         <ChartThree />
+        <ChartTwo />
+
+        <ChartOne />
       </div>
     </>
   );
